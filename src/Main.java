f91f443af7d0ebc25da29701cf01de493409ed51
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-20 08:13:17 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -14,21 +14,34 @@ class Card {
     static final int DIAMONDS = 1;
     static final int HEARTS = 2;
     static final int SPADES = 3;
+    static String[] suits = {"Klöver", "Ruter", "Hjärter", "Spader"};
+    static String[] ranks = {
+            "ess", "två", "tre", "fyra", "fem", "sex", "sju",
+            "åtta", "nio", "tio", "knekt", "dam", "kung"
+    };
 
     int suit;
     int rank;
 
     @Override
     public String toString() {
-
+        return suits[suit] + " " + ranks[rank-1];
     }
 
     @Override
     public boolean equals(Object o) {
-
+        if (o instanceof Card c) {
+            return suit == c.suit && rank == c.rank;
+        }
+        return false;
     }
 
     void change(int suit, int rank) {
-
+        if ((suit >= 0 && suit <= 3) && (rank >= 0 && rank <= 13)) {
+            this.suit = suit;
+            this.rank = rank;
+        } else {
+            throw new IllegalArgumentException("Felaktigt kort");
+        }
     }
 }
